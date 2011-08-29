71e18efa3ec1eba75408bfeabf4d8878f7ef8577
emanuele.bertoldi@gmail.com <emanuele.bertoldi@gmail.com@71535020-02ba-11df-897e-95fb7fca2173>
2011-08-29 07:38:40 +0000

    The "author" field is not editable.
    git-svn-id: http://prometeo-erp.googlecode.com/svn/branches/new-era@300 71535020-02ba-11df-897e-95fb7fca2173

--- projects/forms.py
+++ projects/forms.py
@@ -33,7 +33,7 @@ class ProjectForm(forms.ModelForm):
     """
     class Meta:
         model = Project
-        exclude = ('closed')
+        exclude = ('author', 'closed')
         widgets = {
             'description': CKEditor(),
             'tags': forms.SelectMultipleAndAddWidget(add_url='/tags/add/'),
@@ -45,7 +45,7 @@ class AreaForm(forms.ModelForm):
     """
     class Meta:
         model = Area
-        exclude = ('project')
+        exclude = ('project', 'author')
         widgets = {
             'description': CKEditor(),
             'tags': forms.SelectMultipleAndAddWidget(add_url='/tags/add/'),
@@ -57,7 +57,7 @@ class MilestoneForm(forms.ModelForm):
     """
     class Meta:
         model = Milestone
-        exclude = ('project', 'closed')
+        exclude = ('project', 'author', 'closed')
         widgets = {
             'description': CKEditor(),
             'tags': forms.SelectMultipleAndAddWidget(add_url='/tags/add/'),
