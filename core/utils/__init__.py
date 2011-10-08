18f80eecd07392693e35ab7d64137e67c34052d0
emanuele.bertoldi@gmail.com <emanuele.bertoldi@gmail.com@71535020-02ba-11df-897e-95fb7fca2173>
2011-10-08 16:00:22 +0000

    Merged "core.utils.filter" into "core.utils".
    git-svn-id: http://prometeo-erp.googlecode.com/svn/branches/new-era@436 71535020-02ba-11df-897e-95fb7fca2173

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -20,6 +20,70 @@ __author__ = 'Emanuele Bertoldi <emanuele.bertoldi@gmail.com>'
 __copyright__ = 'Copyright (c) 2011 Emanuele Bertoldi'
 __version__ = '0.0.2'
 
+from django.db import models
+from django.db.models import Q, query
+from django.db.models import fields as django_fields
+from django.utils.datastructures import SortedDict
+
+def filter_objects(request, model_or_queryset=None, fields=[], exclude=[]):
+    matches = []
+    model = None
+    object_list = None
+    queryset = None
+
+    if isinstance(model_or_queryset, models.Model):
+        model = model_or_queryset
+        object_list = model.objects.all()
+
+    elif isinstance(model_or_queryset, query.QuerySet):
+        model = model_or_queryset.model
+        object_list = model_or_queryset
+
+    if not object_list.query.can_filter():
+        pks = [instance.pk for instance in object_list]
+        object_list = model.objects.filter(pk__in=pks)
+    
+    filter_fields = get_filter_fields(request, model, fields, exclude)
+    
+    if request.method == 'POST':
+        queryset = []
+        for f, value in filter_fields:
+            if value is not None:
+                if isinstance(f, django_fields.related.RelatedField):
+                    pass # Fail silently.
+                else:
+                    queryset.append(Q(**{"%s__startswith" % f.name: value}) | Q(**{"%s__endswith" % f.name: value}))
+
+    if (queryset is not None):
+        matches = object_list.filter(*queryset)
+    else:
+        matches = object_list
+
+    try:
+        order_by = request.GET['order_by']
+        matches = matches.order_by(order_by)
+    except:
+        pass
+        
+    return [f.name for f, value in filter_fields], SortedDict(filter_fields), matches
+    
+def get_filter_fields(request, model, fields, exclude):
+    filter_fields = [(f, filter_field_value(request, f)) for f in model._meta.fields if is_visible(f.name, fields, exclude)]
+    return filter_fields
+
+def is_visible(field, fields=[], exclude=[]):
+    return (len(fields) == 0 or field in fields) and field not in exclude
+
+def filter_field_value(request, field):
+    name = field.name
+    if request.POST.has_key("sub_%s" % name):
+        return None
+    elif request.POST.has_key(name):
+        return request.POST[name]
+    elif request.POST.has_key(u'filter_field') and request.POST[u'filter_field'] == name:
+        return request.POST[u'filter_query']
+    return None
+
 def clean_referer(request):
     try:
         referer = request.META['HTTP_REFERER']
