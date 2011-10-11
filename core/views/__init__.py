f6a2c1cdb71e138ae5d8574bb9035ac982d83aa0
emanuele.bertoldi@gmail.com <emanuele.bertoldi@gmail.com@71535020-02ba-11df-897e-95fb7fca2173>
2011-10-11 14:13:52 +0000

    Added default view helpers: "filtered_list_detail" it's a modified version of standard Django's "list_detail" view with support for custom filtering.
    git-svn-id: http://prometeo-erp.googlecode.com/svn/branches/new-era@462 71535020-02ba-11df-897e-95fb7fca2173

---
+++ core/views/__init__.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This file is part of the prometeo project.
+
+This program is free software: you can redistribute it and/or modify it 
+under the terms of the GNU Lesser General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+"""
+
+__author__ = 'Emanuele Bertoldi <emanuele.bertoldi@gmail.com>'
+__copyright__ = 'Copyright (c) 2011 Emanuele Bertoldi'
+__version__ = '0.0.2'
+
+from django.views.generic import list_detail
+
+from prometeo.core.utils import filter_objects
+
+def filtered_list_detail(request, model_or_queryset, fields=[], exclude=[], page=0, paginate_by=10, **kwargs):
+    """Returns a filtered list of given objects.
+    """
+    field_names, filter_fields, object_list = filter_objects(
+        request,
+        model_or_queryset,
+        fields=fields,
+        exclude=exclude
+    )
+
+    extra_context = {
+        'field_names': field_names,
+        'filter_fields': filter_fields,
+    }
+
+    if 'extra_context' in kwargs:
+        extra_context.update(kwargs.pop('extra_context'))
+
+    return list_detail.object_list(
+        request,
+        queryset=object_list,
+        paginate_by=paginate_by,
+        page=page,
+        extra_context=extra_context,
+        **kwargs
+    )
