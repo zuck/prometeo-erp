b52384bc2441427c5d9c8ea832652f4398c231d5
emanuele.bertoldi@gmail.com <emanuele.bertoldi@gmail.com@71535020-02ba-11df-897e-95fb7fca2173>
2011-10-18 23:14:20 +0000

    Added working solution for row-level permissions.
    git-svn-id: http://prometeo-erp.googlecode.com/svn/branches/new-era@539 71535020-02ba-11df-897e-95fb7fca2173

---
+++ core/auth/backends.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This file is part of the prometeo project.
+
+This program is free software: you can redistribute it and/or modify it 
+under the terms of the GNU Lesser General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+"""
+
+__author__ = 'Emanuele Bertoldi <emanuele.bertoldi@gmail.com>'
+__copyright__ = 'Copyright (c) 2011 Emanuele Bertoldi'
+__version__ = '0.0.2'
+
+from django.db.models import Q
+from django.conf import settings
+from django.contrib.auth.models import User, Permission
+
+from models import *
+
+class ObjectPermissionBackend(object):
+    """Backend which enables support for row-level permissions.
+    """
+    supports_object_permissions = True
+    supports_anonymous_user = True
+    supports_inactive_user = True
+
+    def authenticate(self, username, password):
+        return None
+
+    def get_group_permissions(self, user_obj, obj):
+        if not hasattr(user_obj, '_group_obj_perm_cache'):
+            perms = ObjectPermission.objects.filter(groups__user=user_obj, object_id=obj.pk)
+            perms = perms.values_list('perm__content_type__app_label', 'perm__codename').order_by()
+            user_obj._group_obj_perm_cache = set(["%s.%s" % (ct, name) for ct, name in perms])
+        return user_obj._group_obj_perm_cache
+
+    def get_all_permissions(self, user_obj, obj):
+        if user_obj.is_anonymous():
+            return set()
+        if not hasattr(user_obj, '_obj_perm_cache'):
+            user_obj._obj_perm_cache = set([u"%s.%s" % (p.perm.content_type.app_label, p.perm.codename) for p in user_obj.objectpermissions.filter(object_id=obj.pk)])
+            user_obj._obj_perm_cache.update(self.get_group_permissions(user_obj, obj))
+        return user_obj._obj_perm_cache
+
+    def has_perm(self, user_obj, perm, obj=None):
+        """This method checks if the user_obj has perm on obj.
+        """
+        if not user_obj.is_authenticated():
+            user_obj = User.objects.get(pk=settings.ANONYMOUS_USER_ID)
+
+        if not user_obj.is_active:
+            return False
+
+        if obj is None:
+            return False
+
+        return perm in self.get_all_permissions(user_obj, obj)
